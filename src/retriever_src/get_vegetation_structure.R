f7fb4e936f1bdfd1c08aae33ef8632ea5b071769
MarconiS <sergio.marconi@weecology.org>
2018-12-05 16:47:44 -0500

    Update get_vegetation_structure.R

--- src/retriever_src/get_vegetation_structure.R
+++ src/retriever_src/get_vegetation_structure.R
@@ -1,6 +1,6 @@
 get_vegetation_structure <- function(geo_only = T){
   file_tos_coordinates = read_csv("./TOS_retriever/tmp/filesToStack10098/stackedFiles/vst_perplotperyear.csv") %>%
-    select(c("plotID","plotType", "utmZone", "easting", "northing", "coordinateUncertainty", "nlcdClass"))
+    select(c("plotID","plotType", "utmZone", "easting", "northing", "elevation","coordinateUncertainty", "nlcdClass"))
 
   file_mapping = read_csv("./TOS_retriever/tmp/filesToStack10098/stackedFiles/vst_mappingandtagging.csv") %>%
     select(c("uid", "eventID", "domainID","siteID","plotID","subplotID",
@@ -18,8 +18,17 @@ get_vegetation_structure <- function(geo_only = T){
     t %>%
     data.frame
   colnames(coords) <- c('UTM_E', 'UTM_N')
-
-  field_tag <- cbind(dat, coords)
+  
+  #add vegetation structure information
+  vegstr_mapping = read_csv("./TOS_retriever/tmp/filesToStack10098/stackedFiles/vst_apparentindividual.csv") %>%
+    dplyr::select("individualID", "growthForm","plantStatus","stemDiameter", "measurementHeight","height","baseCrownHeight",              
+                   "breakHeight","breakDiameter","maxCrownDiameter", "ninetyCrownDiameter","canopyPosition","shape",                        
+                   "basalStemDiameter","basalStemDiameterMsrmntHeight", "maxBaseCrownDiameter",         
+                  "ninetyBaseCrownDiameter")
+  
+  field_tag <- cbind(dat, coords) 
+  field_tag <- inner_join(vegstr_mapping, field_tag)
+  
   write_csv(field_tag, './TOS_retriever/out/field_data.csv')
   if(geo_only == F){
     return(file_mapping)
