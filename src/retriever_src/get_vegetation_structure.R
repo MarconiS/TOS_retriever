6a2ab8118c2913a7e9db7e29556d700432e246ba
MarconiS <sergio.marconi@weecology.org>
2019-02-25 10:37:29 -0500

    Update get_vegetation_structure.R

--- src/retriever_src/get_vegetation_structure.R
+++ src/retriever_src/get_vegetation_structure.R
@@ -1,42 +1,42 @@
 get_vegetation_structure <- function(geo_only = T){
-  file_tos_coordinates = read_csv("./TOS_retriever/tmp/filesToStack10098/stackedFiles/vst_perplotperyear.csv") %>%
-    select(c("plotID","plotType", "utmZone", "easting", "northing", "elevation","coordinateUncertainty", "nlcdClass"))
   
-  file_mapping = read_csv("./TOS_retriever/tmp/filesToStack10098/stackedFiles/vst_mappingandtagging.csv") %>%
-    select(c("uid", "eventID", "domainID","siteID","plotID","subplotID",
-             "nestedSubplotID","pointID","stemDistance","stemAzimuth",
-             "cfcOnlyTag","individualID","supportingStemIndividualID","previouslyTaggedAs",
-             "taxonID","scientificName"))
-  dat = inner_join(file_mapping,file_tos_coordinates,  by = "plotID") %>%
-    drop_na(stemAzimuth) %>%
-    unique
+  file_mapping = read_csv("./TOS_Retriever/tmp/filesToStack10098/stackedFiles/vst_mappingandtagging.csv") %>%
+    dplyr::select(c("uid", "eventID", "domainID","siteID","plotID","subplotID",
+                    "nestedSubplotID","pointID","stemDistance","stemAzimuth",
+                    "individualID","supportingStemIndividualID","previouslyTaggedAs",
+                    "taxonID","scientificName"))
   
+  plots<-sf::st_read("./TOS_Retriever/dat/All_Neon_TOS_Points_V5.shp")  %>% filter(str_detect(appMods,"vst"))
+  dat<-file_mapping %>% 
+    mutate(pointID=factor(pointID, levels = levels(plots$pointID))) %>% 
+    mutate(plotID=factor(plotID, levels = levels(plots$plotID))) %>% 
+    left_join(plots,by=c("plotID","pointID"))
+  
+  dat <- dat[!is.na(dat$stemAzimuth), ]
   # get tree coordinates
   dat_apply <- dat %>%
-    select(c(stemDistance, stemAzimuth, easting, northing))
+    dplyr::select(c(stemDistance, stemAzimuth, easting, northing)) 
   coords <- apply(dat_apply,1,function(params)from_dist_to_utm(params[1],params[2], params[3], params[4])) %>%
     t %>%
     data.frame
   colnames(coords) <- c('UTM_E', 'UTM_N')
-  
-  # add vegetation structure information
-  max_no_na <- function(x)max(x, na.rm=T)
-  vegstr_mapping = read_csv("./TOS_retriever/tmp/filesToStack10098/stackedFiles/vst_apparentindividual.csv") %>%
-    dplyr::select("individualID", "stemDiameter","height","maxCrownDiameter","basalStemDiameter") %>% 
-    group_by(individualID) %>% summarize_all(max_no_na)
-  vegstr_mapping$stemDiameter[is.infinite(vegstr_mapping$stemDiameter)] <- NA
-  vegstr_mapping$height[is.infinite(vegstr_mapping$height)] <- NA
-  vegstr_mapping$maxCrownDiameter[is.infinite(vegstr_mapping$maxCrownDiameter)] <- NA
-  vegstr_mapping$basalStemDiameter[is.infinite(vegstr_mapping$basalStemDiameter)] <- NA
-
-  #
   field_tag <- cbind(dat, coords)
-  field_tag <- right_join(vegstr_mapping, field_tag, by = "individualID") 
-  field_tag <- field_tag[!is.na(field_tag$UTM_N), ]
-  write_csv(field_tag, './TOS_retriever/out/field_data.csv')
-  if(geo_only == F){
-    return(file_mapping)
-  } else{
-    return(field_tag)
-  }
+  
+  max_no_na <- function(x)max(x, na.rm=T)
+  apparent = read_csv("./TOS_Retriever/tmp/filesToStack10098/stackedFiles/vst_apparentindividual.csv") %>%
+    dplyr::select("individualID", "stemDiameter", "height", "baseCrownHeight", 
+                  "maxCrownDiameter",  "ninetyCrownDiameter") %>%
+    group_by(individualID) %>%
+    summarise_all(funs(max_no_na))
+  
+  apparent$stemDiameter[is.infinite(apparent$stemDiameter)] <- NA
+  apparent$height[is.infinite(apparent$height)] <- NA
+  apparent$maxCrownDiameter[is.infinite(apparent$maxCrownDiameter)] <- NA
+  apparent$ninetyCrownDiameter[is.infinite(apparent$ninetyCrownDiameter)] <- NA
+  
+  crown_attributes = left_join(field_tag, apparent, by="individualID") %>%
+    unique
+  
+  #write_csv(field_tag, './TOS_Retriever/out/field_data_no_attribute.csv')
+  write_csv(crown_attributes, './TOS_Retriever/out/field_data.csv')
 }
